1c91fe9567d64d0329e3ae61724c539cbbc0963a
HowToBeCalculated <HowToBeCalculated@outlook.com>
2022-01-01 18:11:57 -0500

    added some personal touches while reviewing the code

--- Chapter01/falsify_message.py
+++ Chapter01/falsify_message.py
@@ -4,14 +4,19 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
-message = b'Nelson hates cat'
+message = b'Ryan hates Pokemon'
+## checkout 'verify_message.py' to see how a signature usually looks like, obviously this is fake
 signature = b'Fake Signature'
 
-with open("nelsonkey.pub", "rb") as key_file:
+#load in our public key
+with open("ryankey.pub", "rb") as key_file:
     public_key = serialization.load_pem_public_key(
         key_file.read(),
         backend=default_backend())
 
+print("~" * 30, "Attributes of Public Key Object:", public_key.__dict__, "~" * 30, sep='\n')
+
+## verification will fail as signature is not as expected
 public_key.verify(
  signature,
  message,
